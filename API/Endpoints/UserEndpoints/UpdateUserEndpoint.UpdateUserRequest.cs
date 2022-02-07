dae7ec114161f9bff11b22d2104f792a02f7ba8a
Amal Fernando <amal.fernando@jims.net>
2022-02-08 09:07:34 +1100

    Removed old api folder

--- API/Endpoints/UserEndpoints/UpdateUserEndpoint.UpdateUserRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using UserManagementService.Core.UserAggregate;
-
-namespace UserManagementService.API.Endpoints.UserEndpoints;
-public record UpdateCustomerRequest
-{
-    [FromRoute(Name = "id")] public Guid Id { get; init; }
-    [FromBody] public User UpdatedUser { get; set; } = default!;
-}
