1e01d5f12da997893b16e0ef3e9fbd6d4f4c4f60
Amal Fernando <amal.fernando@jims.net>
2022-02-14 10:42:38 +1100

    Refactored

--- UserManagementService.API/Endpoints/SwaggerEndpointDefinition.cs
+++ UserManagementService.API/Endpoints/SwaggerEndpointDefinition.cs
@@ -7,7 +7,7 @@ public class SwaggerEndpointDefinition : IEndpointDefinition
     public void DefineEndpoints(WebApplication app)
     {
         app.UseSwagger();
-        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DotnetDocsShow.MinimalApiTests.Structured v1"));
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "UserManagementService v1"));
     }
 
     public void DefineServices(IServiceCollection services)
@@ -15,7 +15,7 @@ public class SwaggerEndpointDefinition : IEndpointDefinition
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen(c =>
         {
-            c.SwaggerDoc("v1", new OpenApiInfo { Title = "DotnetDocsShow.MinimalApiTests.Structured", Version = "v1" });
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "UserManagementService", Version = "v1" });
         });
     }
 }
